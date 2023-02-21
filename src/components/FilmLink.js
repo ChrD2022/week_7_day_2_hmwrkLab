1d30e35cb64dd5aca77d506a6265f5e5a84ce040
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-02-21 15:15:19 +0000

    submission

---
+++ src/components/FilmLink.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FilmLink = () => {
+    return(
+        <a className="box3" href="https://www.imdb.com/calendar/?region=gb" target="_blank"> View more upcoming releases >> </a>
+    )
+};
+
+export default FilmLink;
